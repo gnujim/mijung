d580f877616f5be98bd406dfc4e269632bc954d6
Mijung <mijung.m@gmail.com>
2018-11-09 01:31:36 -0800

    config change for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+        spaceId: $GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: $GATSBY_CONTENTFUL_DELIVERY_ACCESS_TOKEN,
       },
     },
   ],
