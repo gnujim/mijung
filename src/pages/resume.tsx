5d1f2e53dfab8d8902d111161b235e06ed5594c3
Mijung <mijung.m@gmail.com>
2019-01-17 17:47:44 -0800

    started type structure of resume query

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import { Link } from 'gatsby';
-// import Header from "../components/header"
+import { graphql, StaticQuery, Link } from 'gatsby';
+import styled from 'styled-components';
+import { Header } from '../components/header';
+
+interface ResumeQueryData {
+  data: {
+    allContentfulJsonResume: {
+      edges: Array<{
+        node: {
+          jsonResume: {
+            skills: Array<{
+              title: string;
+              skills: string;
+            }>;
+            projects: Array<{
+              name: string;
+              summary: string;
+              website: string;
+              releaseDate: string;
+            }>;
+            education: Array<{
+              area: string;
+              date: string;
+              institution: string;
+            }>;
+            work: Array<{
+              company: string;
+              summary: string;
+              position: string;
+              website: string;
+              startDate: string;
+              endDate: string;
+            }>;
+          };
+        };
+      }>;
+    };
+  };
+}
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <>
+    <Header />
     <Link to="/">Home</Link>
-    <p>Send us a message!</p>
-  </div>
+  </>
 );
