e90dec49b540cc8ab86eca0e24af5159431154d3
Mijung <mijung.m@gmail.com>
2019-01-22 12:18:59 -0800

    builds structure of queries for resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -3,12 +3,25 @@ import { graphql, StaticQuery, Link } from 'gatsby';
 import styled from 'styled-components';
 import { Header } from '../components/header';
 
+const ResumeContainer = styled.div``;
+
+const ResumeSidebarContainer = styled.div``;
+
+const ResumeBodyContainer = styled.div``;
+
 interface ResumeQueryData {
   data: {
     allContentfulJsonResume: {
       edges: Array<{
         node: {
           jsonResume: {
+            basics: {
+              name: string;
+              phone: string;
+              github: string;
+              linkedin: string;
+              location: string;
+            };
             skills: Array<{
               title: string;
               skills: string;
@@ -36,12 +49,74 @@ interface ResumeQueryData {
         };
       }>;
     };
+    allContenfulAbout: {
+      edges: Array<{
+        node: {
+          aboutText: {
+            aboutText: string;
+          };
+        };
+      }>;
+    };
   };
 }
 
 export default () => (
   <>
-    <Header />
     <Link to="/">Home</Link>
+    <ResumeContainer>
+      <ResumeSidebar />
+      <ResumeBodyContainer>
+        {/* Skills */}
+        {/* Projects */}
+        {/* Education */}
+        {/* Employment */}
+      </ResumeBodyContainer>
+    </ResumeContainer>
   </>
+);
+
+export const ResumeSidebar = () => (
+  <StaticQuery
+    query={graphql`
+      query ResumeSidebarQueryData {
+        allContentfulJsonResume {
+          edges {
+            node {
+              jsonResume {
+                basics {
+                  name
+                  phone
+                  github
+                  linkedin
+                  location
+                }
+              }
+            }
+          }
+        }
+        allContentfulAbout {
+          edges {
+            node {
+              aboutText {
+                aboutText
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <ResumeSidebarContainer>
+        <div>{data.allContentfulJsonResume.edges[0].node.jsonResume.basics.name}</div>
+        <div>{data.allContentfulAbout.edges[0].node.aboutText.aboutText}</div>
+        <div>
+          <div>{data.allContentfulJsonResume.edges[0].node.jsonResume.basics.phone}</div>
+          <div>{data.allContentfulJsonResume.edges[0].node.jsonResume.basics.github}</div>
+          <div>{data.allContentfulJsonResume.edges[0].node.jsonResume.basics.linkedin}</div>
+          <div>{data.allContentfulJsonResume.edges[0].node.jsonResume.basics.location}</div>
+        </div>
+      </ResumeSidebarContainer>
+    )}
+  />
 );
