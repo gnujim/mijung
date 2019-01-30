53dc671e352251295c65440699439d5fd146ee37
Mijung <mijung.m@gmail.com>
2019-01-30 11:56:21 -0800

    replaces googleanalytics plugin for gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,9 +17,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
         trackingId: 'UA-133426620-1',
+        head: false,
+        anonymize: true,
       },
     },
   ],
