1ccb1b80ed82b333789c89d4c28e9d074554d47b
Mijung <mijung.m@gmail.com>
2019-01-30 12:14:39 -0800

    google test

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       resolve: `gatsby-plugin-gtag`,
       options: {
         trackingId: process.env.GOOGLE_TAG,
-        head: true,
+        head: false,
         anonymize: true,
       },
     },
